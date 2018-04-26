6cdb0fb7d2a47fb6e27f9ab6ecc3a5f7a0e97479
Lykos115 <javierherrera52@gmail.com>
2018-04-25 21:44:52 -0700

    Entity class base class for player/enemies?

---
+++ Entity.hpp
@@ -0,0 +1,42 @@
+//
+//  Entity.hpp
+//  CS2A-Project
+//
+//  Created by Lykos on 4/25/18.
+//  Copyright © 2018 Lykos. All rights reserved.
+//
+
+#ifndef Entity_hpp
+#define Entity_hpp
+#include <SFML/Audio.hpp>
+#include <SFML/Graphics.hpp>
+#include <iostream>
+
+using namespace std;
+
+class Entity{
+  
+public:
+    Entity();
+    Entity(sf::RectangleShape rect, sf::Sprite sprite, sf::Text text);
+    sf::RectangleShape getRect() const {return rect;}
+    sf::Sprite getSprite() const {return sprite;}
+    sf::Text getText() const {return text;}
+    
+    void setRect(sf::RectangleShape rect){this -> rect = rect;}
+    void setSprite(sf::Sprite sprite){this -> sprite = sprite;}
+    void setText(sf::Text text){this-> text = text;}
+    
+protected:
+    
+    sf::RectangleShape rect;
+    sf::Sprite sprite;
+    sf::Text text;
+    sf::Texture texture;
+    
+    
+};
+
+#include <stdio.h>
+
+#endif /* Entity_hpp */
