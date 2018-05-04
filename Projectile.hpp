6f87d35911b4b2a324023fb7681b9f29a5c0d780
Lykos115 <javierherrera52@gmail.com>
2018-05-03 17:48:13 -0700

    Projectiles in progress

---
+++ Projectile.hpp
@@ -0,0 +1,31 @@
+//
+//  Projectile.hpp
+//  CS2A-Project
+//
+//  Created by Lykos on 5/3/18.
+//  Copyright © 2018 Lykos. All rights reserved.
+//
+
+#ifndef Projectile_hpp
+#define Projectile_hpp
+
+#include "Entity.hpp"
+
+class Projectile : public Entity{
+    
+public:
+    
+    Projectile();
+    Projectile(sf::Sprite sprite);
+    
+    
+    
+protected:
+    int counter = 0;
+    int AD = 0;
+    
+};
+
+#include <stdio.h>
+
+#endif /* Projectile_hpp */
